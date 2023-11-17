efcc24a9bfcf804e6a820737b2b23dc4b378e820
Tylam.dev <tylam.dev@gmail.com>
2023-11-17 11:15:09 -0500

    Second View Controlled Finished

---
+++ Food_App/Food_App/Controller/RecipeController.swift
@@ -0,0 +1,29 @@
+//
+//  RecipeController.swift
+//  Food_App
+//
+//  Created by Tyrone Vera on 17/11/23.
+//
+
+import UIKit
+
+class RecipeController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+wefwe
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
